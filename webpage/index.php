5801cebb165296add310729b7bee79a9fe4930b7
Muhammad Sahib Khan <Sahib.Khan@irp.org.pk>
2019-02-07 12:18:49 +0500

    new commit

---
+++ webpage/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Example app demo new demo</title>
+</head>
+
+<body class="">
+    hello sahib your test applicaton successfully deployed on aws from github server. testing on demo
+</body>
+
+</html>
